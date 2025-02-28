0611441d0dc96f2908bb5bd852f05738eface781
cho6019 <j01040864205@gmail.com>
2025-02-28 10:37:51 +0900

    d

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from typing import Union
 from fastapi import FastAPI
 import time
 import random
+import numpy as np
 
 app = FastAPI()
-N = 10 **4
+N = 10 **6
 
 @app.get("/")
 def read_root():
@@ -81,34 +82,49 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.get("/add-large-arrays")
 def add_large_arrays():
-    array_creation_time, addition_time = add_arrays(generate_random_array_with_randint, N)
+    array_creation_time, addition_time = add_arrays(generate_random_array_with_choices, N)
     return{
         "array_creation_time": array_creation_time,
         "addition_time": addition_time
     }
+    
 
 
 
+# def add_arrays(generate_random_array, N = 10 **6):
+#     a = generate_random_array(N)
+#     b = generate_random_array(N)
+    
+#     result = a+b
+    
+#     return result
+    
 
 def add_arrays(generate_random_array, N = 10 **6):
-    start_creation_time = time.time()
     a = generate_random_array(N)
     b = generate_random_array(N)
-    end_creation_time = time.time()
     
-    start_cal_time = time.time()
-    result = [x + y for x, y in zip(a, b)]
-    end_cal_time = time.time()
+    result = []
+    for x, y in zip(a, b):
+        result.append(x + y)
     
-    array_creation_time = end_creation_time - start_creation_time
-    addition_time = end_cal_time - start_cal_time
-    return array_creation_time, addition_time
+    return result
     
     
 def generate_random_array_with_randint(N):
     a = [random.randint(0, 100) for _ in range(N)]
     return a
     
-def generate_random_array_with_choices(N):
-    a = [random.choices(range(101), k=N)]
+def generate_random_array_with_numpy(N):
+    a = np.random.choice(range(101), size = N)
     return a
+    
+    
+    
+    
+    
+    
+def generate_random_array_with_choices(N):
+    a = random.choices(range(101), k=N)
+    return a
+
